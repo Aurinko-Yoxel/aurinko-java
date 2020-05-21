f97212f80ad858880ea084313da54b30b0b75c45
Alexey Panteleev <alexey@yoxel.com>
2020-05-21 09:30:55 -0700

    revert forceManaged

--- src/main/java/com/yoxel/aurinko/ServiceUtils.java
+++ src/main/java/com/yoxel/aurinko/ServiceUtils.java
@@ -159,7 +159,7 @@ public class ServiceUtils {
     }
 
     public static AurAccountToken syncAccount(AurinkoService aurinko, UserModelAccess uma, Account acc,
-                                              SyncData sd, ОAuth2ClientRegs appRegs, ServiceTemplate forceManagedBy) {
+                                              SyncData sd, ОAuth2ClientRegs appRegs) { // ServiceTemplate forceManagedBy
         if (!acc.getProtocol().isReadEmail()) {
             return null;
         }
@@ -172,9 +172,8 @@ public class ServiceUtils {
                 aurAcc = fromAccount(acc, uma.getClientUser().getName(), sd, appRegs);
 
         AurAccountToken aurToken = null;
-        if (acc.isTrustServer() && acc.getTemplId() > 0 || forceManagedBy != null) {
-            ServiceTemplate svcTempl = forceManagedBy == null ? uma.getServiceTemplate(acc.getTemplId()) : forceManagedBy;
-
+        if (acc.isTrustServer() && acc.getTemplId() > 0 ) { // || forceManagedBy != null
+            final ServiceTemplate svcTempl = uma.getServiceTemplate(acc.getTemplId());
             if (svcTempl.getAurinkoToken() != null) {
                 System.out.println(
                         "Upserting managed account " + acc.getId() + ", " + acc.getName() + ", " + acc.getEmailAddress() + ", clientOrgId: " + uma.getClientCompany().getExtId());
