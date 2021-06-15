464fd06e165042525289130624f65576558f4dfa
Alexey Panteleev <alexey@yoxel.com>
2021-06-15 14:45:32 -0400

    gmail: better token migration

--- src/main/java/com/yoxel/aurinko/ServiceUtils.java
+++ src/main/java/com/yoxel/aurinko/ServiceUtils.java
@@ -36,7 +36,8 @@ public class ServiceUtils {
     SyncData getSyncData(Account acc);
   }
 
-  private static AurAccountDto fromAccount(Account acc, String userName, SyncData sd, ОAuth2ClientRegs appRegs) {
+  private static AurAccountDto fromAccount(Account acc, String userName, SyncData sd,
+                                           ОAuth2ClientRegs appRegs) {
 
     final List<String> scopes = new ArrayList<>();
     if (acc.isScanEmail()) {
@@ -80,14 +81,14 @@ public class ServiceUtils {
           accDto.setOauthClientId(appRegs.getOAuthClientReg("google.oauth2.client"));
         }
 
-        if (sd.getAccessToken() == null) {
-          if (acc.isImportEvents()) {
+        if (sd.getAccessToken() == null || sd.getRefreshToken() == null) {
+          if (acc.isImportEvents() && sd.getCalRefreshToken() != null) {
             accDto.setAuthString1(sd.getCalAccessToken());
             accDto.setAuthString2(sd.getCalRefreshToken());
-          } else if (acc.isImportContacts()) {
+          } else if (acc.isImportContacts() && sd.getContRefreshToken() != null) {
             accDto.setAuthString1(sd.getContAccessToken());
             accDto.setAuthString2(sd.getContRefreshToken());
-          } else if (acc.isScanEmail()) {
+          } else if (acc.isScanEmail() && sd.getMailRefreshToken() != null) {
             accDto.setAuthString1(sd.getMailAccessToken());
             accDto.setAuthString2(sd.getMailRefreshToken());
           }
@@ -155,14 +156,16 @@ public class ServiceUtils {
       try {
         final GenericJson
             sdData =
-            Utils.getDefaultJsonFactory().createJsonParser(templ.getPassword()).parse(GenericJson.class);
+            Utils.getDefaultJsonFactory().createJsonParser(templ.getPassword())
+                .parse(GenericJson.class);
 
         accDto.setAuthUserId((String) sdData.get("client_id"));
         accDto.setAuthOrgId(authDomain);
         accDto.setServerInfo((String) sdData.get("project_id"));
         accDto.setEmail((String) sdData.get("client_email"));
         accDto.setLoginString((String) sdData.get("private_key_id"));
-        accDto.setAuthString1(SecurityUtils.extractContentsFromPkFile((String) sdData.get("private_key")));
+        accDto.setAuthString1(
+            SecurityUtils.extractContentsFromPkFile((String) sdData.get("private_key")));
       } catch (IOException e) {
         throw new IllegalArgumentException("Bad json");
       }
@@ -185,8 +188,10 @@ public class ServiceUtils {
     return accDto;
   }
 
-  public static AurAccountToken syncAccount(AurinkoService aurinko, UserModelAccess uma, Account acc,
-                                            SyncData sd, ОAuth2ClientRegs appRegs) { // ServiceTemplate forceManagedBy
+  public static AurAccountToken syncAccount(AurinkoService aurinko, UserModelAccess uma,
+                                            Account acc,
+                                            SyncData sd,
+                                            ОAuth2ClientRegs appRegs) { // ServiceTemplate forceManagedBy
     if (!acc.getProtocol().isReadEmail() || acc.getEmailAddress() == null) {
       return null;
     }
@@ -208,7 +213,8 @@ public class ServiceUtils {
 
       final ServiceTemplate svcTempl = uma.getServiceTemplate(acc.getTemplId());
       if (svcTempl.getAurinkoToken() == null) {
-        log.warn("No Aurinko token for the template " + svcTempl.getId() + ", " + svcTempl.getName());
+        log.warn(
+            "No Aurinko token for the template " + svcTempl.getId() + ", " + svcTempl.getName());
         return null;
       }
 
@@ -217,7 +223,8 @@ public class ServiceUtils {
             .createWithAccountAuth(svcTempl.getAurinkoToken())
             .accounts
             .getMe();
-        log.info("Upserting managed account " + acc.getId() + ", " + acc.getName() + ", " + acc.getEmailAddress()
+        log.info("Upserting managed account " + acc.getId() + ", " + acc.getName() + ", " + acc
+            .getEmailAddress()
                  + ", clientOrgId: " + uma.getClientCompany().getExtId());
 
         aurToken = aurinko.accounts.upsertManaged(aurAcc, svcAcc.getId());
@@ -227,7 +234,8 @@ public class ServiceUtils {
 
     } else {
       log.info(
-          "Upserting account " + acc.getId() + ", " + acc.getName() + ", " + acc.getEmailAddress() + ", clientOrgId: "
+          "Upserting account " + acc.getId() + ", " + acc.getName() + ", " + acc.getEmailAddress()
+          + ", clientOrgId: "
           + uma.getClientCompany().getExtId());
 
       try {
@@ -245,7 +253,8 @@ public class ServiceUtils {
     String getAuthString(long sid) throws IOException;
   }
 
-  public static AurAccountToken syncTemplate(AurinkoService aurinko, String clientOrgId, ServiceTemplate svcTempl,
+  public static AurAccountToken syncTemplate(AurinkoService aurinko, String clientOrgId,
+                                             ServiceTemplate svcTempl,
                                              ОAuth2ClientRegs appRegs, AuthServiceAccess authAccess)
       throws IOException {
     if (!svcTempl.getProtocol().isReadEmail() || svcTempl.getPassword() == null) {
@@ -257,7 +266,8 @@ public class ServiceUtils {
     aurAcc.setClientOrgId(clientOrgId + "/" + svcTempl.getGroupId());
 
     log.info(
-        "Upserting service account " + svcTempl.getId() + ", " + svcTempl.getName() + ", clientOrgId: " + clientOrgId
+        "Upserting service account " + svcTempl.getId() + ", " + svcTempl.getName()
+        + ", clientOrgId: " + clientOrgId
         + "/" + svcTempl.getGroupId());
 
     AurAccountToken aurToken = null;
