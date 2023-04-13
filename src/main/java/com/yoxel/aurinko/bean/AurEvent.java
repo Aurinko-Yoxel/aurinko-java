b74c62b2b94a5b2c7b21450a7cc7dcf2524f568c
Lev Sivashov <lev@eml.cc>
2023-04-13 22:49:21 +0300

    AurEvent: isMeeting & OnlineMeetingDetails

--- src/main/java/com/yoxel/aurinko/bean/AurEvent.java
+++ src/main/java/com/yoxel/aurinko/bean/AurEvent.java
@@ -1,7 +1,11 @@
 package com.yoxel.aurinko.bean;
 
 import com.google.api.client.util.Key;
+
 import com.yoxel.aurinko.bean.sub.*;
+
+import org.apache.commons.collections4.CollectionUtils;
+
 import lombok.Data;
 
 import java.util.List;
@@ -11,60 +15,64 @@ import java.util.Map;
 @Data
 public class AurEvent extends AurLiveIdEntity {
 
-    @Key // ("folderId")
-    private String calendarId;
+  @Key // ("folderId")
+  private String calendarId;
 
-    @Key
-    private String subject;
+  @Key
+  private String subject;
 
-    @Key
-    private String description;
+  @Key
+  private String description;
 
-    @Key
-    private String htmlLink;
+  @Key
+  private String htmlLink;
 
-    @Key
-    private String location;
+  @Key
+  private String location;
 
-    @Key
-    private String type;
+  @Key
+  private String type;
 
-    @Key
-    private EventDateTime start;
+  @Key
+  private EventDateTime start;
 
-    @Key
-    private EventDateTime end;
+  @Key
+  private EventDateTime end;
 
-    @Key
-    private Organizer organizer;
+  @Key
+  private Organizer organizer;
 
-    @Key
-    private MeetingInfo meetingInfo;
+  @Key
+  private MeetingInfo meetingInfo;
 
-    @Key
-    private String recurrenceType;
+  @Key
+  private String recurrenceType;
 
-    @Key
-    private Recurrence recurrence;
+  @Key
+  private Recurrence recurrence;
 
-    @Key
-    private OccurrenceInfo occurrenceInfo;
+  @Key
+  private OccurrenceInfo occurrenceInfo;
 
-    @Key
-    private String iCalUId;
+  @Key
+  private String iCalUId;
 
-    @Key
-    private String globalId;
+  @Key
+  private String globalId;
 
-    @Key
-    private String showAs;
+  @Key
+  private String showAs;
 
-    @Key
-    private String sensitivity;
+  @Key
+  private String sensitivity;
 
-    @Key
-    private List<String> categories;
+  @Key
+  private List<String> categories;
 
-    @Key
-    private Map<String, String> nativeProperties;
+  @Key
+  private Map<String, String> nativeProperties;
+
+  public boolean isMeeting() {
+    return meetingInfo != null && CollectionUtils.isNotEmpty(meetingInfo.getAttendees());
+  }
 }
