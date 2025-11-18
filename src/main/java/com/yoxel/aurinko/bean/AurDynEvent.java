5e0d17175546ed54efbbfb6c1daab54a350c435d
Alexey Panteleev <alexey@yoxel.com>
2025-11-18 14:19:47 -0500

    AurDynEventAttendees

--- src/main/java/com/yoxel/aurinko/bean/AurDynEvent.java
+++ src/main/java/com/yoxel/aurinko/bean/AurDynEvent.java
@@ -1,16 +1,40 @@
 package com.yoxel.aurinko.bean;
 
+import com.google.api.client.json.GenericJson;
 import com.google.api.client.util.DateTime;
 import com.google.api.client.util.Key;
 
+import com.yoxel.aurinko.bean.sub.EmailAddress;
+import com.yoxel.aurinko.bean.sub.EventParticipant;
+
+import java.util.List;
 import java.util.Map;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class AurDynEvent extends AurLiveIdEntity implements AurNativePropertiesSupport {
+
+  @Getter
+  @Setter
+  public static class Attendee {
+
+    @Key
+    private String userId;
+
+    @Key
+    private String emailAddress;
+
+    @Key
+    private String type;
+
+    @Key
+    private String status;
+  }
 
   private String calendarId;
 
@@ -46,6 +70,9 @@ public class AurDynEvent extends AurLiveIdEntity implements AurNativePropertiesS
 
   @Key
   private String organizerName;
+
+  @Key
+  private List<Attendee> attendees;
 
   @Key("__nativeProperties")
   private Map<String, Object> nativeProperties;
