3149c5fc442f8a1ddb57968fc0067f67cdddc95d
Anatolii <1284004+tolikr@users.noreply.github.com>
2026-08-03 10:02:36 +0300

    Fix after merge.

--- src/main/java/com/yoxel/aurinko/bean/AurRewriteHtml.java
+++ src/main/java/com/yoxel/aurinko/bean/AurRewriteHtml.java
@@ -2,6 +2,7 @@ package com.yoxel.aurinko.bean;
 
 import com.google.api.client.json.GenericJson;
 import com.google.api.client.util.Key;
+import com.yoxel.aurinko.bean.sub.AurOutEmailTrack;
 import lombok.Data;
 
 @Data
